e1e6fd91254d1c587e98ea7b7b4b4a11ebe4c452
Alys-7 <deprez.alais1@gmail.com>
2026-02-05 11:28:54 +0100

    test(TestInterpreteur)

---
+++ tests/convertisseur/TestInterpreteur.java
@@ -0,0 +1,78 @@
+package convertisseur;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import utils.ComposantsString;
+
+public class TestInterpreteur {
+    private Interpreteur interpreteur1;
+    private Interpreteur interpreteur2;
+    private Interpreteur interpreteur3;
+    private Interpreteur interpreteur4;
+    private Interpreteur interpreteur5;
+
+    @Before
+    public void setUp() {
+        this.interpreteur1 = new Interpreteur("4-50");
+        this.interpreteur2 = new Interpreteur("6+-8");
+        this.interpreteur3 = new Interpreteur("4.5*6.25");
+        this.interpreteur4 = new Interpreteur("-4/50");
+        this.interpreteur5 = new Interpreteur("82--3.5");
+    }
+
+    // Le test 1 vérifie que Interpreteru sépare bien l'expression en trois
+    @Test
+    public void testSeparerElement1() {
+        // Arrange
+        ComposantsString result;
+        // Act
+        result = interpreteur1.separerComposants();
+        // Assert
+        Assert.assertEquals("L'expression devrait être séparée en trois: 4, -, 50", new ComposantsString("4","-","50"), result);
+    }
+
+    // Le test 2 vérifie que Interpreteru sépare bien l'expression en trois, avec le deuxième nombre négatif
+    @Test
+    public void testSeparerElement2() {
+        // Arrange
+        ComposantsString result;
+        // Act
+        result = interpreteur2.separerComposants();
+        // Assert
+        Assert.assertEquals("L'expression devrait être séparée en trois: 6, +, -8", new ComposantsString("6","+","-8"), result);
+    }
+
+    // Le test 3 vérifie que Interpreteru sépare bien l'expression en trois, avec des nombres flottants
+    @Test
+    public void testSeparerElement3() {
+        // Arrange
+        ComposantsString result;
+        // Act
+        result = interpreteur3.separerComposants();
+        // Assert
+        Assert.assertEquals("L'expression devrait être séparée en trois: 4.5, *, 6.25", new ComposantsString("4.5","*","6.25"), result);
+    }
+
+    // Le test 4 vérifie que Interpreteru sépare bien l'expression en trois, avec le premier nombre négatif
+    @Test
+    public void testSeparerElement4() {
+        // Arrange
+        ComposantsString result;
+        // Act
+        result = interpreteur4.separerComposants();
+        // Assert
+        Assert.assertEquals("L'expression devrait être séparée en trois: -4, /, 50", new ComposantsString("-4","/","50"), result);
+    }
+
+    // Le test 5 vérifie que Interpreteru sépare bien l'expression en trois, avec le deuxième nombre négatif et une soustraction
+    @Test
+    public void testSeparerElement5() {
+        // Arrange
+        ComposantsString result;
+        // Act
+        result = interpreteur5.separerComposants();
+        // Assert
+        Assert.assertEquals("L'expression devrait être séparée en trois: 82, -, -3.5", new ComposantsString("82","-","-3.5"), result);
+    }
+}
