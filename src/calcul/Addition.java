13a2500f3de32a16733cabed37ff7576ba676661
Dorian Weber <dorianweb57@gmail.com>
2026-02-05 09:44:18 +0100

    commentaire ajouter package calcul, interpreteur

--- src/calcul/Addition.java
+++ src/calcul/Addition.java
@@ -1,21 +1,30 @@
 package calcul;
 
 public class Addition implements IMath{
-
+    // addition implements IMath et va récupérer ses propriétés et méthods.
     private double valeur1;
     private double valeur2;
 
+    /**
+     * Constructeur de la class Addition prend pour paramètres les valeur1 et valeur2 en type double.
+     * @param : valeur1
+     * @param : valeur2
+     */
     public Addition(double valeur1, double valeur2) {
         this.valeur1 = valeur1;
         this.valeur2 = valeur2;
     }
 
+    /**
+     * Méthod qui permet d'additionner la valeur1 à la valeur2.
+     * @return résultat en double.
+     */
     @Override
     public double calculer() {
         return valeur1 + valeur2;
     }
 
-    // pour test
+    // Mise en place des Setters pour Tests
 
     public void setValeur1(double valeur1) {
         this.valeur1 = valeur1;
--- src/calcul/Division.java
+++ src/calcul/Division.java
@@ -3,21 +3,33 @@ package calcul;
 import exception.DivisionParZero;
 
 public class Division implements IMath {
-
+    // Division implements IMath et va récupérer ses propriétés et méthods.
 
     private double valeur1;
     private double valeur2;
+
+    /**
+     * Constructeur de la class Division prend pour paramètres les valeur1 et valeur2 en type double.
+     * @param : valeur1
+     * @param : valeur2
+     */
 
     public Division(double valeur1, double valeur2) {
         this.valeur1 = valeur1;
         this.valeur2 = valeur2;
     }
 
+    /**
+     * Méthod qui permet de diviser la valeur1 par la valeur2.
+     * @return résultat en double.
+     */
     @Override
     public double calculer() {
         if (valeur2 == 0) throw new DivisionParZero("Impossible de diviser par zéro");
         return valeur1 / valeur2;
     }
+
+    // Mise en place des Setters pour Tests
 
     public void setValeur1(double valeur1) {
         this.valeur1 = valeur1;
