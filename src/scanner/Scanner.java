e16f12d17ec08017455c98ecad864aa84650cad5
unknown <benoit.champlon79@gmail.com>
2026-02-05 10:07:08 +0100

    feat(scanner): changement sur la facon de gerer la verification d'erreur, avec une methode principale qui appelle chaque test

--- src/scanner/Scanner.java
+++ src/scanner/Scanner.java
@@ -1,10 +1,12 @@
 package scanner;
 
+import exception.AlaisException;
 import exception.FormatIncorrect;
+import exception.OperateurInconnu;
 import exception.ValeurNonNumerique;
 
 import static scanner.VerifierExpression.verifierExpression;
-import static scanner.VerifierExpression.verifierValeurNumeriques;
+
 
 public class Scanner {
     private java.util.Scanner scanner;
@@ -20,27 +22,13 @@ public class Scanner {
             expression = scanner.nextLine();
 
             try {
-                if (!verifierValeurNumeriques(expression)) {
-                    throw new ValeurNonNumerique("Veuillez entrer des nombres et des operateurs uniquement");
-
-                }
-
-            }
-            catch (ValeurNonNumerique e) {
+                verifierExpression(expression);
+            } catch (OperateurInconnu | AlaisException | ValeurNonNumerique  | FormatIncorrect e) {
                 System.out.println(e.getMessage());
                 continue;
             }
-
-            try {
-                if (!verifierExpression(expression)) {
-                    throw new FormatIncorrect("Le format doit suivre le schéma 'nombre opérateur nombre'");
-                }
-                break;
-            } catch (FormatIncorrect e) {
-                System.out.println(e.getMessage());
-            }
+            break;
         }
-
         return expression;
     }
 
--- src/scanner/VerifierExpression.java
+++ src/scanner/VerifierExpression.java
@@ -1,9 +1,18 @@
 package scanner;
 
+import exception.AlaisException;
+import exception.FormatIncorrect;
+import exception.OperateurInconnu;
+import exception.ValeurNonNumerique;
+
+
 public class VerifierExpression {
 
-    public static boolean verifierExpression(String expression) {
-        return expression.matches("^(\\s*(-\\s*)?\\d+(\\.\\d+)?\\s*)(\\+|-|\\*|/)(\\s*-?\\s*\\d+(\\.\\d+)?\\s*)$");
+    public static void verifierExpression(String expression) {
+        if (!verifierOperateur(expression)) throw new OperateurInconnu("Il manque un opéraeur valide a l'expression entrée.");
+        if (verifierVirgule(expression)) throw new AlaisException("Veuillez utiliser un point à la place de la virgule pour séparer les décimales.");
+        if (!verifierValeurNumeriques(expression)) throw new ValeurNonNumerique("L'expression doit contenir uniquement des valeurs numériques et l'opérateur.");
+        if (!verifierFormat(expression)) throw new FormatIncorrect("Le format de l'expression devrait suivre le format 'valeur opérateur valeur");
     }
 
     public static boolean verifierFormat(String expression) {
@@ -16,5 +25,9 @@ public class VerifierExpression {
 
     public static boolean verifierOperateur(String expression) {
         return expression.matches("^.+(\\+|\\-|\\*|\\/).*$");
+    }
+
+    public static boolean verifierVirgule(String expression) {
+        return expression.matches(",");
     }
 }
