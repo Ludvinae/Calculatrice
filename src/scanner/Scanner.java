f8a678f897cfab2cfdacdbeb13f51d700de3d574
Dorian Weber <dorianweb57@gmail.com>
2026-02-05 11:57:36 +0100

    ajout de commentaire package scanner

--- src/scanner/Scanner.java
+++ src/scanner/Scanner.java
@@ -7,20 +7,27 @@ import exception.ValeurNonNumerique;
 
 import static scanner.VerifierExpression.verifierExpression;
 
-
+// La class Scanner va permettre à l'utilisateur de saisir une expression
 public class Scanner {
     private java.util.Scanner scanner;
 
+    /**
+     * Constructeur de la class Scanner
+     */
     public Scanner() {
         scanner = new java.util.Scanner(System.in);
     }
 
+    /**
+     * Method qui demande à l'utilisateur une expression et boucle si l'expression est incorrect
+     * @return une expression en String
+     */
     public String demanderExpression() {
         String expression = "";
         while (true) {
             System.out.print("Veuillez saisir une expression mathématique: ");
             expression = scanner.nextLine();
-
+            // Ici remonte toutes les erreurs liées à l'expression entrée par l'utilisateur et sont catch.
             try {
                 verifierExpression(expression);
             } catch (OperateurInconnu | AlaisException | ValeurNonNumerique  | FormatIncorrect e) {
@@ -32,6 +39,9 @@ public class Scanner {
         return expression;
     }
 
+    /**
+     * Methods qui ferme le Scanner après utilisation
+     */
     public void fermerScanner() {
         scanner.close();
     }
--- src/scanner/VerifierExpression.java
+++ src/scanner/VerifierExpression.java
@@ -5,9 +5,12 @@ import exception.FormatIncorrect;
 import exception.OperateurInconnu;
 import exception.ValeurNonNumerique;
 
-
+// La class VerifierExpression va permettre de vérifier que l'expression soit correct avant de commencer les calculs.
 public class VerifierExpression {
-
+    /**
+     * Methods qui vérifie l'expression et les éventuelles erreurs et renvoyant l'exception adéquate
+     * @param : expression
+     */
     public static void verifierExpression(String expression) {
         if (!verifierOperateur(expression)) throw new OperateurInconnu("Il manque un opéraeur valide a l'expression entrée.");
         if (verifierVirgule(expression)) throw new AlaisException("Veuillez utiliser un point à la place de la virgule pour séparer les décimales.");
@@ -15,18 +18,38 @@ public class VerifierExpression {
         if (!verifierFormat(expression)) throw new FormatIncorrect("Le format de l'expression devrait suivre le format 'valeur opérateur valeur");
     }
 
+    /**
+     * Method qui vérifie que le format x+y est bien respecté, si true le format est correct.
+     * @param : expression
+     * @return Boolean
+     */
     public static boolean verifierFormat(String expression) {
         return expression.matches("^(\\s*(-\\s*)?\\d+(\\.\\d+)?\\s*)(\\+|-|\\*|/)(\\s*-?\\s*\\d+(\\.\\d+)?\\s*)$");
     }
 
+    /**
+     * Method qui vérifie que les valeurs soient bien numérique, si true l'expression ne contient que les caractères autorisés.
+     * @param : expression
+     * @return Boolean
+     */
     public static boolean verifierValeurNumeriques(String expression) {
         return expression.matches("^[\\d\\.\\s\\+\\-\\*\\/]*$");
     }
 
+    /**
+     * Method qui vérifie la présence d'un moins un opérateur valide, si true l'opérateur est valide.
+     * @param : expression
+     * @return Boolean
+     */
     public static boolean verifierOperateur(String expression) {
         return expression.matches("^.+(\\+|\\-|\\*|\\/).*$");
     }
 
+    /**
+     * Method qui vérifie la présence du caractère ",", si true l'expression n'est pas valide.
+     * @param : expression
+     * @return Boolean
+     */
     public static boolean verifierVirgule(String expression) {
         if (expression.contains(",")) return true;
         return false;
