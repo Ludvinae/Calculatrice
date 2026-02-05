46c3a916cece37734ab34498f3c24d177b57e896
Dorian Weber <dorianweb57@gmail.com>
2026-02-05 11:18:53 +0100

    ajout commentaire ValeurEnDouble

--- src/convertisseur/ValeurEnDouble.java
+++ src/convertisseur/ValeurEnDouble.java
@@ -1,9 +1,13 @@
 package convertisseur;
-
+    // La classe ValeurEnDouble sert à convertir valeur d'un String à un Double.
 public class ValeurEnDouble {
 
     private Double valeur;
 
+        /**
+         * Methods qui convertit un String en Double
+         * @param : valeur
+         */
     public ValeurEnDouble(String valeur) {
         this.valeur = (Double.parseDouble(valeur));
     }
