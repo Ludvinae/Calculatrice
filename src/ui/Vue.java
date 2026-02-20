8d251eef0c20a38e1fef7b9dbd8d0d3faeb4f5e3
Ludvinae <benoit.champlon79@gmail.com>
2026-02-20 14:18:33 +0100

    feat(ui): Changement de structure pour la Vue, hérite maintenant de JFrame au lieu de Canvas

--- src/ui/Vue.java
+++ src/ui/Vue.java
@@ -3,30 +3,33 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
-public class Vue extends Canvas {
+public class Vue extends JFrame {
 
     private final int width = 350;
-    private final int height = 500;
+    private final int height = 480;
 
     public Vue() {
         setSize(width, height);
+        setName("Calculatrice");
+        // Permet de fermer le programme lorsque l'on ferme la fenetre de l'UI
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
-        JFrame frame = new JFrame("Calculatrice");
-        frame.pack();
-        frame.setSize(width, height);
+        // Panneau d'affichage
+        JPanel affichage = new JPanel();
+        affichage.setPreferredSize(new Dimension(width, 100));
+        setBackground(Color.white);
+        // Positionne l'affichage en haut
+        add(affichage, BorderLayout.NORTH);
 
-        Panel panel = new Panel();
-        frame.add(this);
-        frame.setContentPane(panel);
+        // Panneau des touches
+        Panel touches = new Panel();
+        setBackground(Color.BLACK);
+        // Positionne les touches au milieu
+        add(touches, BorderLayout.CENTER);
 
-        this.setFocusable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.setIgnoreRepaint(true);
-        frame.setResizable(false);
-
-        frame.setVisible(true);
-
+        // Ne pas ajouter de composants apres cette commande
+        setVisible(true);
 
         lancer();
     }
