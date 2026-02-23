6dbc26da97c7cfc8a4308e1adac034ecdf804605
Ludvinae <benoit.champlon79@gmail.com>
2026-02-23 10:42:09 +0100

    docs(ui): ajout de JavaDoc dans ControleurUI

--- src/ui/ControleurUI.java
+++ src/ui/ControleurUI.java
@@ -5,7 +5,7 @@ import controleur.Manager;
 import javax.swing.*;
 
 /**
- * Ecoute les boutons et met a jour Vue
+ * Fait le lien entre le Manager et la Vue
  */
 public class ControleurUI {
 
@@ -22,6 +22,10 @@ public class ControleurUI {
         initialiserListeners();
     }
 
+    /**
+     * Ajoute un ecouteur sur les boutons de la calculatrice
+     * Appelle gererEntree avec le contenu du texte des boutons convertis
+     */
     private void initialiserListeners() {
         // Itere sur la liste de boutons du panel touches
         for (JButton bouton : vue.getBoutons()) {
@@ -34,8 +38,11 @@ public class ControleurUI {
         }
     }
 
+    /**
+     * Appelle des méthodes spécifiques en fonction du bouton pressé
+     * @param texte : récupere le texte associé aux boutons (converti en texte utilisable)
+     */
     private void gererEntree(String texte) {
-
         switch (texte) {
             case "=" -> calculer();
             case "Clear" -> {
@@ -54,16 +61,19 @@ public class ControleurUI {
                 vue.changeSigne(); // a implementer pour gerer le changement de signe
             }
             default -> {
-                if (entrees.estTropLong()) return;
-                if (entrees.estVide()) vue.effacerAffichage();
+                if (entrees.estTropLong()) return; // Bloque la longueur de l'entrée pour eviter de sortir de l"ecran
+                if (entrees.estVide()) vue.effacerAffichage(); // Efface l'affichage quand on commence un nouveau calcul
                 entrees.ajouter(texte);
                 vue.afficher(texte);
             }
         }
     }
 
+    /**
+     * Envoi l'expression saisi au Manager pour recuperer le résultat
+     * Puis fait parevenir le résultat a l'affichage
+     */
     private void calculer() {
-
         String expression = entrees.construire();
         String resultat = manager.faireCalculUI(expression);
 
